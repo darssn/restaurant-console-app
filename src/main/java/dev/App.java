355c44732193ad01e3d9ba99ca3fff92a7126446
Darcy NGUYEN <nguyendarcy@hotmail.fr>
2020-01-16 23:52:16 +0100

    TP01 Config xml

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,32 +1,26 @@
 package dev;
 
-import dev.dao.PlatDaoFichier;
-import dev.dao.PlatDaoMemoire;
 import dev.ihm.Menu;
-import dev.service.PlatServiceVersion1;
-import dev.service.PlatServiceVersion2;
+
 
 import java.util.Scanner;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
     public static void main(String[] args) {
+    	
+    	ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config.xml");
 
-        Scanner scanner = new Scanner(System.in);
+      
 
-        PlatDaoMemoire platDaoMemoire = new PlatDaoMemoire();
-
-        // TODO adapter le chemin du fichier (utiliser un répertoire existant)
-        PlatDaoFichier platDaoFichier = new PlatDaoFichier("/home/rossi/Temp/restaurant.txt");
-
-        PlatServiceVersion1 platServiceVersion1 = new PlatServiceVersion1(platDaoFichier);
-        PlatServiceVersion2 platServiceVersion2 = new PlatServiceVersion2(platDaoMemoire);
-
-        Menu menu = new Menu(scanner, platServiceVersion1);
+        Menu menu = context.getBean(Menu.class);
+       
 
         menu.afficher();
-
-        scanner.close();
+        context.getBean(Scanner.class).close();
+        context.close();
 
 
     }
